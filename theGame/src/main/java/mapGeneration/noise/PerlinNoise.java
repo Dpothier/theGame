e380a65e624ed27fc9af4adff8906d676c8fae2e
dpothier <dpothier@live.ca>
2014-06-19 17:04:47 -0400

    Plus de Perlin Noise

--- theGame/src/main/java/mapGeneration/noise/PerlinNoise.java
+++ theGame/src/main/java/mapGeneration/noise/PerlinNoise.java
@@ -5,11 +5,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+import mapGeneration.data.Gradient;
+import mapGeneration.data.Gradients;
+
 public class PerlinNoise {
 	private static final int PERMUTATION_TABLE_SIZE = 256;
 	
 	private List<Integer> permutationTable = new ArrayList<>();
-	private int[][] gradients = new int[8][2];
+	private Gradients possibleGradients = new Gradients();
+
+	private int squareSize;
 	
 	public PerlinNoise(){
 		generatePermutationTable();
@@ -25,11 +30,58 @@ public class PerlinNoise {
 			permutationTable.add(i);
 		}
 		Collections.shuffle(permutationTable);
+		for(int i =0; i < PERMUTATION_TABLE_SIZE; i++){
+			permutationTable.add(i);
+		}
 	}
 
 
-	public int[][] generateHeigthMap(){
-		return null;
+	public double[][] generateHeigthMap(int heigth, int width, int squareSize){
+		this.squareSize = squareSize;
+		double[][] heigthMap = new double[heigth][width];
+		for(int i = 0; i < heigth; i++){
+			for(int j = 0; i < width; j++){
+				heigthMap[i][j] = generatePixel(i, j);
+			}
+		}
+		return heigthMap;
+		
+	}
+
+	private double generatePixel(int i, int j) {
+		double x = (double)i /squareSize;
+		double y = (double)j /squareSize;
+		
+		int x0 = (int) x;
+		int y0 = (int) y;
+		int ii = i % PERMUTATION_TABLE_SIZE;
+		int jj = j % PERMUTATION_TABLE_SIZE;
+		Gradient[] gradients = new Gradient[4];
+		gradients[0] = possibleGradients.getGradient(permutationTable.get(ii + permutationTable.get(jj)) % 8);
+		gradients[1] = possibleGradients.getGradient(permutationTable.get(ii + permutationTable.get(jj + 1)) % 8);
+		gradients[2] = possibleGradients.getGradient(permutationTable.get(ii + 1 + permutationTable.get(jj)) % 8);
+		gradients[3] = possibleGradients.getGradient(permutationTable.get(ii + 1 + permutationTable.get(jj + 1)) % 8);
+		
+		double s = ponderer(x-x0, y-y0, gradients[0]);
+		double t = ponderer(x-(x0+1), y-(y0), gradients[1]);
+		double u = ponderer(x-x0, y-(y0+1), gradients[2]);
+		double v = ponderer(x-(x0+1), y-(y0+1), gradients[3]);
+		
+		double tmp = x-x0;
+	    double Cx = 3 * tmp * tmp - 2 * tmp * tmp * tmp;
+
+	    double Li1 = s + Cx*(t-s);
+	    double Li2 = u + Cx*(v-u);
+
+	     tmp = y - y0;
+	    double Cy = 3 * tmp * tmp - 2 * tmp * tmp * tmp;
+
+	    return Li1 + Cy*(Li2-Li1);
+
+	}
+
+	private double ponderer(double x, double y, Gradient gradient) {
+		return gradient.x * x + gradient.y * y;
 		
 	}
 	
