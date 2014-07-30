0900914829551cf1c412e616fe9e6abd487949b2
dpothier <dpothier@live.ca>
2014-07-30 10:11:35 -0400

    Augmentation de la couverture de tests. Retrait des reference a quad adventure

---
+++ theGame/src/test/java/theGame/UI/Triggers/ArrowTriggerTest.java
@@ -0,0 +1,100 @@
+package theGame.UI.Triggers;
+
+import static org.mockito.Mockito.verify;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.lwjgl.input.Keyboard;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import theGame.engine.input.event.Event;
+import theGame.engine.input.event.Peripheral;
+import theGame.gameServices.MoveFrameService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ArrowTriggerTest {
+	private static final int A_SPEED = 5;
+
+	@Mock
+	private MoveFrameService service;
+	
+	ArrowTrigger trigger;
+	
+	@Before
+	public void setUp(){
+		trigger = new ArrowTrigger(service, A_SPEED);
+	}
+	
+	@Test
+	public void Pressing_Up_Key_Accelerate_In_Positive_Y(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_UP, true, 0, 0, 0, 0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(service).accelerate(0, A_SPEED);
+	}
+	
+	@Test
+	public void Releasing_Up_Key_Deccelarate_In_Positive_Y(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_UP, false, 0,0,0,0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(service).deccelerate(0, A_SPEED);
+	}
+	
+	@Test
+	public void Pressing_Down_Key_Accelerate_In_Negative_Y(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_DOWN, true, 0, 0, 0, 0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(service).accelerate(0, -A_SPEED);
+	}
+	
+	@Test
+	public void Releasing_Down_Key_Deccelarate_In_Negative_Y(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_DOWN, false, 0,0,0,0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(service).deccelerate(0, -A_SPEED);
+	}
+	
+	@Test
+	public void Pressing_Right_Key_Accelerate_In_Positive_X(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_RIGHT, true, 0, 0, 0, 0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(service).accelerate(A_SPEED, 0);
+	}
+	
+	@Test
+	public void Releasing_Right_Key_Deccelarate_In_Positive_X(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_RIGHT, false, 0,0,0,0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(service).deccelerate(A_SPEED, 0);
+	}
+	
+	@Test
+	public void Pressing_Left_Key_Accelerate_In_Negative_X(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_LEFT, true, 0, 0, 0, 0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(service).accelerate(-A_SPEED, 0);
+	}
+	
+	@Test
+	public void Releasing_Left_Key_Deccelarate_In_Negative_X(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_LEFT, false, 0,0,0,0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(service).deccelerate(-A_SPEED, 0);
+	}
+}
---
+++ theGame/src/test/java/theGame/UI/Triggers/ZoomingTriggerTest.java
@@ -0,0 +1,46 @@
+package theGame.UI.Triggers;
+
+import static org.mockito.Mockito.verify;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.lwjgl.input.Keyboard;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import theGame.UI.Zoom;
+import theGame.engine.input.event.Event;
+import theGame.engine.input.event.Peripheral;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ZoomingTriggerTest {
+	
+	@Mock
+	Zoom zoom;
+	
+	ZoomingTrigger trigger;
+	
+	@Before
+	public void SetUp(){
+		trigger = new ZoomingTrigger(zoom);
+	}
+	
+	@Test
+	public void Pressing_A_Key_Zooms(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_A, true, 0,0,0,0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(zoom).zoom();
+	}
+	
+	@Test
+	public void Pressing_Z_Key_Zooms(){
+		Event event = new Event(Peripheral.Keyboard, Keyboard.KEY_Z, true, 0,0,0,0);
+		
+		trigger.reactToEvent(event);
+		
+		verify(zoom).unzoom();;
+	}
+
+}
